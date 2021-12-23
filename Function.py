72204b03ec3cb93e86704b4dd13fe1969bd858a5
jewoos2921 <jewoos2921@gmail.com>
2021-12-23 12:11:22 +0900

    step 7

--- Function.py
+++ Function.py
@@ -5,6 +5,17 @@ class Variable:
     def __init__(self, data):
         self.data = data
         self.grad = None  # 기울기
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        f = self.creator  # 1. 함수를 가져온다
+        if f is not None:
+            x = f.input  # 2. 함수의 입력을 가져온다.
+            x.grad = f.backward(self.grad)  # 3. 함수의 backward 메서드를 호출한다.
+            x.backward()  # 하나 앞 변수의 backward 메서드를 호출한다 (재귀).
 
 
 class Function:
@@ -12,9 +23,11 @@ class Function:
         x = input.data  # 데이터를 꺼낸다.
         # y = x ** 2  # 실제 계산
         y = self.forward(x)
-        out = Variable(y)  # Variable 형태로 되돌린다.
+        output = Variable(y)  # Variable 형태로 되돌린다.
+        output.set_creator(self)  # 출력 변수에 창조자를 설정한다.
         self.input = input  # 입력 변수를 기억(보관)한다.
-        return out
+        self.output = output  # 출력도 저장한다.
+        return output
 
     def forward(self, x):
         raise NotImplementedError()
